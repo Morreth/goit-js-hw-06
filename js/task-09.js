0502fa564be92329c1de82bd2c69ba7250874fed
Morreth <47885094+Morreth@users.noreply.github.com>
2022-11-15 14:07:43 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+
+const changeColorBtn = document.querySelector(".change-color");
+const nameOfColor = document.querySelector(".color");
+const body = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", changeRandomColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeRandomColor() {
+  const rndCol = getRandomHexColor();
+  body.style.backgroundColor = rndCol;
+  nameOfColor.textContent = rndCol;
 }
